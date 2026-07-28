9c71b04938c982922428b43a24a367a824968668
Nguyen-tuan-dat <nguyentuandat31082007@gmail.com>
2026-07-28 09:23:10 +0700

    Update bai_tap_day04.py

--- bai_tap_day04.py
+++ bai_tap_day04.py
@@ -12,13 +12,11 @@ orders = [
     {"id": "DH04", "total": "8500000", "discount_code": "VIP20", "is_vip": True}
 ]
 
-# BÀI 1
+# Bài1:
 def validate_registration_input(name, email, phone):
-    # Chuẩn hóa dữ liệu
     name = name.strip()
     email = email.strip().lower()
     phone = phone.strip()
-
     errors = []
     if "@" not in email:
         errors.append("Thiếu '@'")
@@ -28,40 +26,32 @@ def validate_registration_input(name, email, phone):
         errors.append("SĐT phải có 10 số")
     elif not phone.startswith(("03", "05", "07", "08", "09")):
         errors.append("Sai đầu số VN")
-
     return name, email, phone, errors
-
-
-print("===== BÀI 1 =====")
+print("===== Bài1 =====")
 print("BÁO CÁO CHUẨN HÓA & VALIDATE THÔNG TIN ĐĂNG KÝ")
-
 for i, student in enumerate(raw_registers, start=1):
     name, email, phone, errors = validate_registration_input(
         student["name"],
         student["email"],
         student["phone"]
     )
-
     if len(errors) == 0:
         print(f"[{i}] {name} | Email: {email} | SĐT: {phone} -> Trạng thái: HỢP LỆ")
     else:
         print(f"[{i}] {name} | Email: {email} | SĐT: {phone} -> Trạng thái: KHÔNG HỢP LỆ ({', '.join(errors)})")
 
 
-# BÀI 2
+# Bài2:
 def safe_process_invoice(order_id, raw_total, discount_code, is_vip):
     try:
         total = float(raw_total)
-
         # Chiết khấu
         discount = 0
-
         if is_vip:
             if discount_code == "VIP10":
                 discount = total * 0.10
             elif discount_code == "VIP20":
                 discount = total * 0.20
-
         amount_after_discount = total - discount
 
         # VAT 10%
@@ -88,15 +78,12 @@ def safe_process_invoice(order_id, raw_total, discount_code, is_vip):
                 f"VAT 10%: {vat:,.0f} -> "
                 f"Tổng: {final_total:,.0f} VNĐ [{invoice_type}]"
             )
-
     except ValueError:
         print(
             f"Xử lý lỗi [{order_id}]: "
             f"Số tiền '{raw_total}' không hợp lệ! Bỏ qua đơn hàng."
         )
-
-
-print("\n===== BÀI 2 =====")
+print("\n===== Bài2 =====")
 print("BÁO CÁO XỬ LÝ HÓA ĐƠN AN TOÀN (TRY-EXCEPT & VAT)")
 for order in orders:
     safe_process_invoice(
